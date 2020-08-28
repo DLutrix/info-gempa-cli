3d9bf50495f586ab582c43a4bedca9aef768cfcd
w0rm1995 <meld.mee@gmail.com>
2020-08-28 19:11:13 +0800

    Remove unused variable

--- src/base.py
+++ src/base.py
@@ -6,12 +6,4 @@ class Base:
     def __init__(self, url):
         self.response = requests.get(url)
         self.root = ElementTree.fromstring(self.response.content)
-        self.date = []
-        self.time = []
-        self.coordinates = []
-        self.lat = []
-        self.long = []
-        self.magnitude = []
-        self.depth = []
-        self.location = []
         self.data = []
